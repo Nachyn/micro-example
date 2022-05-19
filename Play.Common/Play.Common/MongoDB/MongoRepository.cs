171cbf1a27cfc9e749b13eb1edfbd59f30cde28a
Nachyn <n.duuza@mw-space.com>
2022-05-20 02:01:09 +0700

    Init

---
+++ Play.Common/Play.Common/MongoDB/MongoRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Play.Common.MongoDB
+{
+    public class MongoRepository<T> : IRepository<T> where T : IEntity
+    {
+        private readonly IMongoCollection<T> _dbCollection;
+        private readonly FilterDefinitionBuilder<T> _filterBuilder = Builders<T>.Filter;
+
+        public MongoRepository(IMongoDatabase database, string collectionName)
+        {
+            _dbCollection = database.GetCollection<T>(collectionName);
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAllAsync()
+        {
+            return await _dbCollection.Find(_filterBuilder.Empty).ToListAsync().ConfigureAwait(false);
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _dbCollection.Find(filter).ToListAsync().ConfigureAwait(false);
+        }
+
+        public async Task<T> GetAsync(Guid id)
+        {
+            var filter = _filterBuilder.Eq(e => e.Id, id);
+            return await _dbCollection.Find(filter).FirstOrDefaultAsync().ConfigureAwait(false);
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _dbCollection.Find(filter).FirstOrDefaultAsync().ConfigureAwait(false);
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            await _dbCollection.InsertOneAsync(entity).ConfigureAwait(false);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            var filter = _filterBuilder.Eq(e => e.Id, entity.Id);
+            await _dbCollection.ReplaceOneAsync(filter, entity).ConfigureAwait(false);
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            var filter = _filterBuilder.Eq(e => e.Id, id);
+
+            await _dbCollection.DeleteOneAsync(filter).ConfigureAwait(false);
+        }
+    }
+}
